4373a206a4729d57c94f81ada57b3c9d7711d674
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-10-19 10:56:47 -0700

    babel setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Empezamos");
+import React from "react";
+import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("app"));
+root.render(
+  <App />
+);
